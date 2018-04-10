8a7698c0d8e4436bddec662a03f8cf6bfbe55484
jaheid <jaheid@ttu.ee>
2018-04-10 16:18:27 +0300

    small edits

---
+++ aurelia/src/notes/notes.js
@@ -0,0 +1,56 @@
+import {HttpClient, json} from 'aurelia-fetch-client'
+export class Welcome {
+	noteData = {}
+	noteList = []
+	activate() {
+		let client = new HttpClient();
+
+		client.fetch('http://localhost:8080/note')
+			.then(response => response.json())
+			.then(notes => this.noteList = notes);
+	}
+
+	addNote() {
+		let client = new HttpClient();
+
+		client.fetch('http://localhost:8080/notes/add', {
+			'method': "POST",
+			'body': json(this.noteData)
+		})
+			.then(response => response.json())
+			.then(data => {
+				console.log("Server saatis " + data.title);
+				document.getElementById('id03_note').style.display='none'
+		});
+
+		console.log("Method executed!")
+	}
+	removeNote(noteId){
+      let client = new HttpClient();
+
+      client.fetch('http://localhost:8080/notes/' + noteId, {
+        'method': 'DELETE'
+      }).then(() => {
+        	
+			console.log("kustutas ");
+
+      });
+  	}
+
+  
+
+
+}
+
+var modal3 = document.getElementById('id03_note');
+var modal4 = document.getElementById('id04_reminder');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal3){
+		modal3.style.display = "none";
+	} else if (event.target == modal4){
+		modal4.style.display = "none";
+	}
+
+}
